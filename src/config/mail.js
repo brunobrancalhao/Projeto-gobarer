5dc0b6df3bca43c6ff4b0d69e85b0ff7b3c19689
Bruno Brancalhao <brunobrancalhao26@gmail.com>
2019-08-19 23:32:01 -0300

    finishing API

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "c5b9bf2c237d37",
+    pass: "fd83e8973248e7"
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>'
+  }
+};
